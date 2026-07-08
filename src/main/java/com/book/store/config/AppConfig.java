08d36c6833bde8584f5fd670d3dd21e4aa3007b9
Oleg <thekveik@gmail.com>
2026-07-09 00:15:12 +0300

    create base structure

---
+++ src/main/java/com/book/store/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.book.store.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackages = "com.book.store")
+public class AppConfig {
+}
