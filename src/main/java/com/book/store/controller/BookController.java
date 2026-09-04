c66611c490afe6c54b5980e02ea9764f27a09145
oleg <oleg.artinc@gmail.com>
2026-09-04 16:04:09 +0300

    add "searchBooks" endpoint

--- src/main/java/com/book/store/controller/BookController.java
+++ src/main/java/com/book/store/controller/BookController.java
@@ -2,12 +2,14 @@ package com.book.store.controller;
 
 import com.book.store.dto.BookDto;
 import com.book.store.dto.CreateBookRequestDto;
+import com.book.store.model.BookSearchParameters;
 import com.book.store.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -30,6 +32,11 @@ public class BookController {
     @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.getById(id);
+    }
+
+    @GetMapping("/search")
+    public List<BookDto> searchBooks(@ModelAttribute BookSearchParameters searchParameters) {
+        return bookService.search(searchParameters);
     }
 
     @PostMapping
