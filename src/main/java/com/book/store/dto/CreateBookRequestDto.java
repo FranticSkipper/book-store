49676ea61bf14e00931e1e98c056fd46919db9fe
oleg <oleg.artinc@gmail.com>
2026-09-23 16:46:21 +0300

    change @Min to @Positive in CreateBookRequestDto.java

--- src/main/java/com/book/store/dto/CreateBookRequestDto.java
+++ src/main/java/com/book/store/dto/CreateBookRequestDto.java
@@ -1,8 +1,8 @@
 package com.book.store.dto;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import lombok.Data;
 
@@ -18,7 +18,7 @@ public class CreateBookRequestDto {
     private String isbn;
 
     @NotNull
-    @Min(0)
+    @Positive
     private BigDecimal price;
     private String description;
     private String coverImage;
