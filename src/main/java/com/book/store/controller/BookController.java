86dbaeee88b5c7b9777944a62d1b53603a623389
oleg <oleg.artinc@gmail.com>
2026-09-24 19:53:14 +0300

    add pagination

--- src/main/java/com/book/store/controller/BookController.java
+++ src/main/java/com/book/store/controller/BookController.java
@@ -5,8 +5,9 @@ import com.book.store.dto.BookSearchParameters;
 import com.book.store.dto.CreateBookRequestDto;
 import com.book.store.service.BookService;
 import jakarta.validation.Valid;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,8 +27,8 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping
-    public List<BookDto> getAll() {
-        return bookService.findAll();
+    public Page<BookDto> getAll(Pageable pageable) {
+        return bookService.findAll(pageable);
     }
 
     @GetMapping("/{id}")
@@ -36,8 +37,10 @@ public class BookController {
     }
 
     @GetMapping("/search")
-    public List<BookDto> searchBooks(@ModelAttribute BookSearchParameters searchParameters) {
-        return bookService.search(searchParameters);
+    public Page<BookDto> searchBooks(
+            @ModelAttribute BookSearchParameters searchParameters,
+            Pageable pageable) {
+        return bookService.search(searchParameters, pageable);
     }
 
     @PostMapping
